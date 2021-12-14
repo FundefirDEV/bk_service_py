fe17f305fdbc7b14301ad1cafc435f0f3fc9c694
AlexanderRaydan <alexraydancode@gmail.com>
2021-12-14 14:42:16 -0300

    ping service

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,17 @@
 """Main URLs module."""
 
+# Django
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
+# Views
+from .views import PingAPIView
+
 urlpatterns = [
     # Django Admin
     path(settings.ADMIN_URL, admin.site.urls),
-    # path('' , include(('bk_service.users.urls' , 'circles') , namespace = 'users')),
+    path('ping', PingAPIView.as_view(),),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
