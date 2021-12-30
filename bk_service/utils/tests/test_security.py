1b50fbdd210b276253bd7a1c13401fab3e413c88
Gibran Raydan <superpegorino@gmail.com>
2021-12-30 15:01:45 -0500

    security test post get

---
+++ bk_service/utils/tests/test_security.py
@@ -0,0 +1,29 @@
+#  Django REST Framework
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.exceptions import ErrorDetail
+from rest_framework.test import APIClient
+
+
+def security_test_get(self, URL):
+    """ Verify 401 return of GET request"""
+    client = APIClient()
+    request = client.get(URL, format='json')
+    body = request.data
+    self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+    self.assertEqual(body,
+                     {'detail': ErrorDetail(
+                         string='Authentication credentials were not provided.', code='not_authenticated')}
+                     )
+
+
+def security_test_post(self, URL):
+    """ Verify 401 return of POST request"""
+    client = APIClient()
+    request = client.post(URL, format='json')
+    body = request.data
+    self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
+    self.assertEqual(body,
+                     {'detail': ErrorDetail(
+                         string='Authentication credentials were not provided.', code='not_authenticated')}
+                     )
