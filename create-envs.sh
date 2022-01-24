f3d800fd708093271f849e63f1d379bf4a8e7f5b
AlexanderRaydan <alexraydancode@gmail.com>
2022-01-24 13:23:34 -0300

    fix create envs command

--- create-envs.sh
+++ create-envs.sh
@@ -13,26 +13,29 @@ envdir=.envs/.production
 mkdir -m 777 $envdir
 cd $envdir
 touch -m .django .postgres .caddy
+echo "*************************************"
+echo $ENV
+echo "*************************************"
 
 # Set postgres env
-echo "POSTGRES_HOST$ENV=\${{secrets.POSTGRES_HOST}}" >> .postgres
-echo "POSTGRES_PORT$ENV=\${{secrets.POSTGRES_PORT}}" >> .postgres
-echo "POSTGRES_DB$ENV=\${{secrets.POSTGRES_DB}}" >> .postgres
-echo "POSTGRES_USER$ENV=\${{secrets.POSTGRES_USER}}" >> .postgres
-echo "POSTGRES_PASSWORD$ENV=\${{secrets.POSTGRES_PASSWORD}}" >> .postgres
+echo "POSTGRES_HOST=\${{secrets.POSTGRES_HOST$ENV}}" >> .postgres
+echo "POSTGRES_PORT=\${{secrets.POSTGRES_PORT$ENV}}" >> .postgres
+echo "POSTGRES_DB=\${{secrets.POSTGRES_DB$ENV}}" >> .postgres
+echo "POSTGRES_USER=\${{secrets.POSTGRES_USER$ENV}}" >> .postgres
+echo "POSTGRES_PASSWORD=\${{secrets.POSTGRES_PASSWORD$ENV}}" >> .postgres
 
 # Set django env
-echo "DJANGO_SETTINGS_MODULE$ENV=\${{secrets.DJANGO_SETTINGS_MODULE}}" >> .django
-echo "DJANGO_SECRET_KEY$ENV=\${{secrets.DJANGO_SECRET_KEY}}" >> .django
-echo "DJANGO_ADMIN_URL$ENV=\${{secrets.DJANGO_ADMIN_URL}}" >> .django
-echo "DJANGO_ALLOWED_HOSTS$ENV=\${{secrets.DJANGO_ALLOWED_HOSTS}}" >> .django
-echo "REDIS_URL$ENV=\${{secrets.REDIS_URL}}" >> .django
-echo "CELERY_FLOWER_USER$ENV=\${{secrets.CELERY_FLOWER_USER}}" >> .django
-echo "CELERY_FLOWER_PASSWORD$ENV=\${{secrets.CELERY_FLOWER_PASSWORD}}" >> .django
+echo "DJANGO_SETTINGS_MODULE=\${{secrets.DJANGO_SETTINGS_MODULE$ENV}}" >> .django
+echo "DJANGO_SECRET_KEY=\${{secrets.DJANGO_SECRET_KEY$ENV}}" >> .django
+echo "DJANGO_ADMIN_URL=\${{secrets.DJANGO_ADMIN_URL$ENV}}" >> .django
+echo "DJANGO_ALLOWED_HOSTS=\${{secrets.DJANGO_ALLOWED_HOSTS$ENV}}" >> .django
+echo "REDIS_URL=\${{secrets.REDIS_URL$ENV}}" >> .django
+echo "CELERY_FLOWER_USER=\${{secrets.CELERY_FLOWER_USER$ENV}}" >> .django
+echo "CELERY_FLOWER_PASSWORD=\${{secrets.CELERY_FLOWER_PASSWORD$ENV}}" >> .django
 
 # # Set caddy envs
-echo "POSTGRES_HOST$ENV=\${{secrets.POSTGRES_HOST}}" >> .caddy
-echo "POSTGRES_PORT$ENV=\${{secrets.POSTGRES_PORT}}" >> .caddy
-echo "POSTGRES_DB$ENV=\${{secrets.POSTGRES_DB}}" >> .caddy
-echo "POSTGRES_USER$ENV=\${{secrets.POSTGRES_USER}}" >> .caddy
-echo "POSTGRES_PASSWORD$ENV=\${{secrets.POSTGRES_PASSWORD}}" >> .caddy
+echo "POSTGRES_HOST=\${{secrets.POSTGRES_HOST$ENV}}" >> .caddy
+echo "POSTGRES_PORT=\${{secrets.POSTGRES_PORT$ENV}}" >> .caddy
+echo "POSTGRES_DB=\${{secrets.POSTGRES_DB$ENV}}" >> .caddy
+echo "POSTGRES_USER=\${{secrets.POSTGRES_USER$ENV}}" >> .caddy
+echo "POSTGRES_PASSWORD=\${{secrets.POSTGRES_PASSWORD$ENV}}" >> .caddy
