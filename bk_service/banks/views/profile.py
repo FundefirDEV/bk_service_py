2f01460ad9562536e816fd5290e9423cbd057ebd
AlexanderRaydan <alexraydancode@gmail.com>
2022-02-21 16:10:34 -0300

    partner guest temporal country

--- bk_service/banks/views/profile.py
+++ bk_service/banks/views/profile.py
@@ -7,12 +7,21 @@ from rest_framework.response import Response
 # Serializer
 from bk_service.banks.serializers import PartnerDetailModelSerializer
 
+# Models
+from bk_service.locations.models import Country
+
 
 class ProfileAPIView(APIView):
     def get(self, request, *args, **kwargs):
 
         partner = request.user.get_partner()
-        country = partner.user.city.state.country
+
+        # TODO this is temporal
+        try:
+            country = partner.user.city.state.country
+        except:
+            country = Country.objects.first()
+
         country_data = {
             'id': country.id,
             'name': country.name,
