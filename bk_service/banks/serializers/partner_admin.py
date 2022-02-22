c1043e6c8c0cf4fe2e6a4703baff20cd70665300
AlexanderRaydan <alexraydancode@gmail.com>
2022-02-22 14:13:11 -0300

    add partner admin v1!

--- bk_service/banks/serializers/partner_admin.py
+++ bk_service/banks/serializers/partner_admin.py
@@ -14,8 +14,7 @@ from bk_service.banks.models.partners import Partner
 from bk_service.utils.enums.banks import PartnerType
 
 # Utils Errors
-from bk_service.utils.constants_errors import build_error_message
-from bk_service.utils.constants_errors import *
+from bk_service.utils.constants_errors import build_error_message, AT_LEAST_ONE_ADMIN
 from bk_service.utils.exceptions_errors import CustomException
 
 
@@ -33,23 +32,12 @@ class PartnersAdminSerializer(serializers.ModelSerializer):
         model = Partner
         fields = ('id', 'role')
 
-    def update_admin(partner_id, role, bank):
+    def update_admin(self, partner_id, role, bank):
         partners_admins = Partner.objects.filter(bank=bank, role=PartnerType.admin, is_active=True)
         valid_partners_admins = partners_admins.exclude(pk=partner_id)
-        # import pdb
-        # pdb.set_trace()
+
         if len(valid_partners_admins) == 0:
             raise CustomException(error=AT_LEAST_ONE_ADMIN)
         partner = get_object_or_404(Partner, pk=partner_id, bank=bank)
         partner.role = role
         partner.save()
-
-    # def validate_admin_body(data, bank):
-    #     # import pdb
-    #     # pdb.set_trace()
-    #     if admin == False:
-    #         ids = [p['id'] for p in data if 'id' in data]
-    #         partners = Partner.objects.filter(
-    #             bank=bank,
-    #             is_active=True,
-    #         )
--- bk_service/banks/views/partner_admin.py
+++ bk_service/banks/views/partner_admin.py
@@ -2,14 +2,11 @@
 # Django REST Framework
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Models
-from bk_service.banks.models import Partner, PartnerDetail
 # Enums
 from bk_service.utils.enums.banks import PartnerType
 # Serializer
 from bk_service.banks.serializers.partner_admin import PartnersAdminSerializer
 # Errors
-from bk_service.utils.constants_errors import build_error_message
 from bk_service.utils.exceptions_errors import CustomException
 from bk_service.utils.constants_errors import PARTNER_IS_NOT_ADMIN
 
@@ -21,13 +18,13 @@ class PartnerAdminApiView(APIView):
         data = request.data
         if partner.role != PartnerType.admin:
             raise CustomException(error=PARTNER_IS_NOT_ADMIN)
+
         serializer = PartnersAdminSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         validated_data = dict(serializer.validated_data)
 
-        # import pdb
-        # pdb.set_trace()
         PartnersAdminSerializer.update_admin(
+            self=self,
             partner_id=validated_data['id'],
             role=validated_data['role'],
             bank=bank)
